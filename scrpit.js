69f1c2ff60068ee00be83413be2db46529fdac3e
Ramisha2000 <158768985+Ramisha2000@users.noreply.github.com>
2024-04-26 11:12:13 +0600

    Add files via upload

--- scrpit.js
+++ scrpit.js
@@ -1,35 +1,54 @@
 function connect() {
+    var searchTerm = document.getElementById("searchBox").value;
+    var url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchTerm}`;
 
-    var searchTerm = document.getElementById("searchBox").value ;
-
-    document.getElementById("searchBox").value = ""; 
-    var url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchTerm}`; 
-
-    fetch (url)
-    .then (res=> res.json() )
-    .then( data => show(data.meals))
+    fetch(url)
+        .then(res => res.json())
+        .then(data => show(data.meals))
+        .catch(err => console.error('Error fetching data:', err));
 }
 
-function show (data) {
- console.log (data); 
- 
- var oldContent = document.getElementById("container");
-oldContent.textContent = ""; 
+function show(data) {
+    if (!data) {
+        console.log("No data found.");
+        return;
+    }
 
- for (var i=1; i<data.length; i++){
+    var container = document.getElementById("container");
+    container.innerHTML = ""; // Clear previous results
 
-    var newDiv = document.createElement("div");
-    newDiv.innerHTML = `Meal Title: <b>${data[i-1].strMeal}</b> <br>
-                        <img src="${data[i-1].strMealThumb}"> <br>
-                        Meal Name:<b>${data[i-1].strMeal}</b> <br>
-                        Meal ID: <b>${data[i-1].idMeal}</b> <br>
-                        Cooking Instructions: ${data[i-1].strInstructions}<br><br>`; 
-    newDiv.classList.add("innerStyle");
-    oldContent.appendChild(newDiv); 
-    
- }
+    // Determine the number of meals to display
+    let mealsToShow = data.length > 5 ? 5 : data.length;
 
- 
+    for (let i = 0; i < mealsToShow; i++) {
+        let meal = data[i];
+        var newDiv = document.createElement("div");
+        newDiv.innerHTML = `Meal Title: <b>${meal.strMeal}</b> <br>
+                            <img src="${meal.strMealThumb}" alt="Meal Image"> <br>
+                            Meal Name: <b>${meal.strMeal}</b> <br>
+                            Meal ID: <b>${meal.idMeal}</b> <br>
+                            Cooking Instructions: ${meal.strInstructions}<br><br>`;
+        newDiv.classList.add("innerStyle");
+        container.appendChild(newDiv);
+    }
 
+    if (data.length > 5) {
+        var showAllButton = document.createElement("button");
+        showAllButton.textContent = "SHOW ALL";
+        showAllButton.onclick = function () {
+            container.innerHTML = ""; // Clear displayed meals
 
+            data.forEach(meal => {
+                var allMealsDiv = document.createElement("div");
+                allMealsDiv.innerHTML = `Meal Title: <b>${meal.strMeal}</b> <br>
+                                         <img src="${meal.strMealThumb}" alt="Meal Image"> <br>
+                                         Meal Name: <b>${meal.strMeal}</b> <br>
+                                         Meal ID: <b>${meal.idMeal}</b> <br>
+                                         Cooking Instructions: ${meal.strInstructions}<br><br>`;
+                allMealsDiv.classList.add("innerStyle");
+                container.appendChild(allMealsDiv);
+            });
+        };
+        container.appendChild(showAllButton);
+    }
 }
